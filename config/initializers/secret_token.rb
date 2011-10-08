6ec5bad3ca5b265cfa14416b25099b1305058668
El Skwid (Don Morrison) <elskwid@gmail.com>
2011-10-07 23:15:11 -0700

    Init rails app and bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Madeinreno::Application.config.secret_token = 'db7a398c00f5e15bd1064bf1eab3a7e816829bc5133cf7219d250abb0e9894311ff793e3541649bdfb5ade839771e6bcf960eabe8a5ee1a93f5d694f9d40f0ca'
